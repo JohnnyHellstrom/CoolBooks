fbcc5f7c1570d515aebffc52527f4ac3269b7239
JohnnyHellstrom <124794710+JohnnyHellstrom@users.noreply.github.com>
2023-04-11 20:33:28 +0200

    diddlar med review index

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,3 +1,16 @@
 <x-layout>
-   <h1> review page </h1>
+   <h1 class="text-4xl"> review page / book view </h1>
+   <h3 class="text-2xl" style="margin-bottom:10rem"> Display hero partial here? </h3>
+   
+   <div class="flex flex-wrap">
+      <div class="w-1/2 flex flex-col">
+         <span>Post new review</span>
+         <span>View my review</span>
+      </div>
+      <div class="w-1/2">
+         <span>Loop all reviews</span>
+      </div>
+
+   </div>
+
 </x-layout>
