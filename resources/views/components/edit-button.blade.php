0fa4c312b1d35f03e33aa512ca6992060c249f51
annaedvardsson <124760772+annaedvardsson@users.noreply.github.com>
2023-04-12 15:17:36 +0200

    View and Edit buttons WITHOUT @csrf and @model

---
+++ resources/views/components/edit-button.blade.php
@@ -0,0 +1,3 @@
+<button class="text-blue-400"><i class="fa-solid fa-pen-to-square" aria-hidden="true"></i>
+   {{$slot}}
+</button>
---
+++ resources/views/components/view-button.blade.php
@@ -0,0 +1,3 @@
+<button class="text-yellow-500"><i class="fa fa-eye" aria-hidden="true"></i>
+   {{$slot}}
+</button>
