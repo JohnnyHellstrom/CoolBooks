a6154e75b03f836ee1526b4ecb3054a326b7180f
Meluttn <88746745+Meluttn@users.noreply.github.com>
2023-04-12 11:48:09 +0200

    fotter fixad

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -62,7 +62,7 @@
 </main>
 
 <footer
-    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-emerald-300 text-white h-24 mt-24 opacity-90 md:justify-center">
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-emerald-400 text-white h-24 mt-24 opacity-90 md:justify-center">
     <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
     <a href="/" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Home page</a>
 </footer>
