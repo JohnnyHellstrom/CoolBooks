8f682f86420fb4642dacf56b5e41f26bfe84f8fb
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2023-04-18 13:24:41 +0200

    sista

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -9,13 +9,21 @@
             <img src="{{$books->image ? asset('storage/' . $books->image) : asset('images/no-image.png')}}" >            
         </div>         
         <div class="flex flex-col w-full">
-            <h3 class="text-3xl font-bold mb-4">Book Information</h3>
-            <div class="text-lg space-y-6">
-                <p>Description: {{$books->description}}  </p>
-                <p>Author: </p>
-                                             
-            </div>             
-            <x-books-tags :tagsCsv="$books->tags" />   
+            <h3 class="text-3xl font-bold mb-4 text-center">Book Information</h3>
+            <div class="flex flex-col text-lg items-center justify-center">
+                <p>Description: {{$books->description}}  </p>             
+                @foreach($books->authors as $bookauthor)
+                    <div class="flex items-center justify-center">
+                        <img class="w-36" src="{{$bookauthor->image ? asset('storage/' . $bookauthor->image) : asset('images\elephpant-running-78x48.gif')}}"> 
+                    </div>
+                    
+
+                    <p>Author: {{$bookauthor->first_name ." " . $bookauthor->last_name}}</p>
+                    <p>Biography: {{$bookauthor->biography}}</p>                
+                @endforeach      
+                                   
+                <x-books-tags :tagsCsv="$books->tags"/>   
+            </div>       
             <div>
                 <div class="mr-10">
                     @include('reviews.all-reviews')
