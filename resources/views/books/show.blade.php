9837976831423c83f34c15353fce7fa9d9e4fd40
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2023-04-20 08:27:23 +0200

    titel fix

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout>    
     <a href="/books" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i>Back to All Books</a>   
-    <h3 class="text-3xl mb-2 text-center">Headline: {{$books->title}}</h3>  
+    <h2 class="text-3xl mb-4 text-center font-bold">{{$books->title}}</h2>  
      
     <div class="flex items-stretch">
         <div class="flex w-full items-center justify-center">
@@ -12,9 +12,7 @@
                 <p>Description: {{$books->description}}  </p> 
                 <p>Genre: {{$books->genres['name']}} </p>            
                 @foreach($books->authors as $bookauthor)                  
-                    <a class="self-center w-55 py-2 px-16 rounded-full text-white-400 bg-gradient-to-r from-cyan-500 to-blue-500" href="/authors/{{$bookauthor->id}}">
-                        {{$bookauthor->first_name . " " . $bookauthor->last_name}}
-                    </a>
+                    <a href="/authors/{{$bookauthor->id}}">{{$bookauthor->first_name . " " . $bookauthor->last_name}}</a>
                     <p>Biography: {{$bookauthor->biography}}</p>                
                 @endforeach      
                                    
@@ -23,7 +21,7 @@
         </div>
     </div>
     <div class="mr-10 mt-20">
-        <h2 class="text-3xl font-bold mb-4 text-center">johnny's stuff</h2>
+        <h3 class="text-3xl font-bold mb-4 text-center">johnny's stuff</h3>
         @include('reviews.all-reviews')
      </div>        
 </x-layout>
