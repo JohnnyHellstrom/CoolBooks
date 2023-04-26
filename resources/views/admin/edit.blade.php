5f3195649fc284533b5e554e2c9cb4f413946930
Erik <88746745+Meluttn@users.noreply.github.com>
2023-04-26 12:50:22 +0200

    admin

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
-    {{ $users->name }}
+
 
     <header class="text-center">
-        <h2 class="text-2xl font-bold uppercase mb-1">Edit User</h2>
+        <h2 class="text-2xl font-bold uppercase mb-1">Edit User: {{ $users->name }}</h2>
     </header>
 
     <div class="flex justify-center md:justify-center">
@@ -13,9 +13,11 @@
 
             <div class="mb-6">
                 <label for="role_id" class="inline-block text-lg mb-2">Role</label>
-                <p>1: Admin 2: User</p>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="role_id"
-                    placeholder="E.g. Jane" value="{{ $users->role_id }}" />
+                <select name="role_id">
+                    <option value="1">Admin</option>
+                    <option value="2">User</option>
+                    {{-- <option value="fiat">Fiat</option> --}}
+                </select>
                 @error('role_id')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -29,3 +31,14 @@
     </div>
 
 </x-layout>
+
+{{-- <p>1: Admin 2: User</p>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="role_id"
+                    placeholder="E.g. Jane" value="{{ $users->role_id }}" />
+
+<select id="cars" name="cars">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="fiat">Fiat</option>
+    <option value="audi">Audi</option>
+  </select> --}}
